bc1c17aecdc00328f6338947c5bf7641ce78ba99
joaovertelo <joaovertelo@gmail.com>
2018-12-17 01:00:11 -0200

    Updates

---
+++ precache-manifest.8942e7cbb42fdc398f78fa23c63690ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/timeline-dito/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "56013c8e3f35054d17c5",
+    "url": "/timeline-dito/static/js/runtime~main.56013c8e.js"
+  },
+  {
+    "revision": "549f027be73eaaf0ed2d",
+    "url": "/timeline-dito/static/js/main.549f027b.chunk.js"
+  },
+  {
+    "revision": "7af84098e26c19df79da",
+    "url": "/timeline-dito/static/js/1.7af84098.chunk.js"
+  },
+  {
+    "revision": "549f027be73eaaf0ed2d",
+    "url": "/timeline-dito/static/css/main.2342534c.chunk.css"
+  },
+  {
+    "revision": "24da5145f2390b42f73417d05841c236",
+    "url": "/timeline-dito/index.html"
+  }
+];
