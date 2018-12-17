ebe9526e259594a3d8c6fb6aa7d589cd3df7540f
joaovertelo <joaovertelo@gmail.com>
2018-12-16 22:41:22 -0200

    recuperacao e manipulacao dos dados

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Timeline from './component/Timeline'
+import axios from 'axios'
+const _ = require('lodash')
+
+class App extends Component {
+  state = {}
+
+  componentDidMount() {
+    axios.get("https://storage.googleapis.com/dito-questions/events.json").then(res => {
+      if (res.data && res.data.events) {
+        let compras = _.filter(res.data.events, { event: "comprou" });
+
+        let compra_produto = _.filter(res.data.events, { event: "comprou-produto" });
+        compra_produto = compra_produto.map(o => {
+          let transaction = o.custom_data.filter(o => o.key === "transaction_id")[0].value
+          let produto_nome = o.custom_data.filter(o => o.key === "product_name")[0].value
+          let produto_preco = o.custom_data.filter(o => o.key === "product_price")[0].value
+
+          return {
+            produto_nome: produto_nome,
+            transaction: transaction,
+            produto_preco: produto_preco,
+            timestamp: o.timestamp
+          };
+        })
+
+        compras = compras.map(o => {
+          let transaction = o.custom_data.filter(o => o.key === "transaction_id")[0].value
+          let loja = o.custom_data.filter(o => o.key === "store_name")[0].value
+          let produtos = _.filter(compra_produto, { transaction })
+
+          return {
+            loja: loja,
+            transaction: transaction,
+            produtos,
+            timestamp: o.timestamp
+          };
+        })
+        compras = _.orderBy(compras, ['timestamp'], ['desc']);
+        this.setState({ compras })
+      }
+    })
+  }
+
+  render() {
+    console.log(this.state.compras)
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+
+        <section >
+          <article>
+            <Timeline />
+          </article>
+        </section>
+
+
+      </div>
+    );
+  }
+}
+
+export default App;
